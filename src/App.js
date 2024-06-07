d1360a358966a716ae3f81b0fc7546c44c9568f3
Shibaz98 <shibaz98@gmail.com>
2024-06-07 16:01:59 +0100

    started on SearchResult component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React, { useState } from 'react';
 import SearchBar from './searchBar/searchBar';
+import SearchResult from './SearchResult/SearchResult';
 
 function App() {
 
@@ -14,6 +15,9 @@ const search = (term) =>{
     <div className="App">
       <h1>React Pokedex-v2</h1>
       <SearchBar search={search}/> 
+      <div className='SearchResultSection'>
+        <SearchResult/>
+      </div>
     </div>
   );
 }
---
+++ src/SearchResult/SearchResult.jsx
@@ -0,0 +1,10 @@
+import './SearchResult.css'; 
+
+function SearchResult(){
+    return(
+        <div className="SearchResultContainer">
+        </div>
+    );
+};
+
+export default SearchResult; 
